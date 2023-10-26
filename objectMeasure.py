3c64413ee51b796b34fd725efad2882c8ca1ea2d
kazem198 <k_gh198@yahoo.com>
2023-10-26 22:33:45 +0330

    code complited

--- objectMeasure.py
+++ objectMeasure.py
@@ -2,34 +2,55 @@ import cv2
 import numpy as np
 import utils
 
-webCam=False
-path="./images/2022011014150371.jpg"
-cap=cv2.VideoCapture(0)
-cap.set(10,160) #cv2.CAP_PROP_BRIGHTNESS
-cap.set(3,1920)
-cap.set(4,1080)
+webCam = False
+path = "./images/2022011014150371.jpg"
+cap = cv2.VideoCapture(0)
+cap.set(10, 160)  # cv2.CAP_PROP_BRIGHTNESS
+cap.set(3, 1920)
+cap.set(4, 1080)
+scale = 3
+wP = 210 * scale
+hP = 297 * scale
 
 
 while True:
-        if webCam:
-          success,img=cap.read()
-        else:
-                img=cv2.imread(path)
-        img,finlaContours=utils.getContours(img,showCanny=True,draw=True)
-        
-        
-        img=cv2.resize(img,(0,0),None,0.5,0.5) 
-                
-        cv2.imshow("original",img)
-        
-        if cv2.waitKey(1) & 0xFF==ord("q"):
-                cv2.destroyAllWindows()
-                break
+    if webCam:
+        success, img = cap.read()
+    else:
+        img = cv2.imread(path)
 
-        
-	
-                
-	        
-               
-		
-		
+    imgCountours, conts = utils.getContours(img, minArea=50000, filter=4)
+
+    if len(conts) != 0:
+        biggest = conts[0][2]
+        imgWrap = utils.wrapImg(img, biggest, wP, hP)
+
+        imgContours2, conts2 = utils.getContours(
+            imgWrap, minArea=2000, filter=4, cThr=[50, 50], draw=True)
+
+        if len(conts) != 0:
+            for obj in conts2:
+                cv2.polylines(imgContours2, [obj[2]], True, (0, 255, 0), 2)
+                nPoints = utils.reorder(obj[2])
+                print(nPoints[0])
+                nW = round(
+                    (utils.findDis(nPoints[0][0]//scale, nPoints[1][0]//scale)/10), 1)
+                nH = round(
+                    (utils.findDis(nPoints[0][0]//scale, nPoints[2][0]//scale)/10), 1)
+                cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[1][0][0], nPoints[1][0][1]),
+                                (255, 0, 255), 3, 8, 0, 0.05)
+                cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[2][0][0], nPoints[2][0][1]),
+                                (255, 0, 255), 3, 8, 0, 0.05)
+                x, y, w, h = obj[3]
+                cv2.putText(imgContours2, '{}cm'.format(nW), (x + 30, y - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,
+                            (255, 0, 255), 2)
+                cv2.putText(imgContours2, '{}cm'.format(nH), (x - 70, y + h // 2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,
+                            (255, 0, 255), 2)
+        cv2.imshow('A4', imgContours2)
+
+    img = cv2.resize(img, (0, 0), None, 0.5, 0.5)
+    cv2.imshow('Original', img)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        cv2.destroyAllWindows()
+        break
